9d72ccd37f17862c4001b6e8ff1d32154c644114
Hasnat Amin <62244945+hasnat7amin@users.noreply.github.com>
2023-12-16 02:59:22 +0500

    Update save_template_section.js

--- src/pages/professional/profile/template/save_template_section.js
+++ src/pages/professional/profile/template/save_template_section.js
@@ -15,7 +15,7 @@ export default function SaveTemplateSection({id,type}) {
   return (
     <section className="w-full">
       <div className="w-full flex items-center justify-center">
-        <button disabled={loading} onClick={handleSave} className={`${loading ? "bg-gray-100  flex items-center justify-center" : "bg-customGreen"} text-white  w-max mx-auto py-3 px-20 shadow-lg  mt-20 mb-10  rounded-full text-base font-medium`} >
+        <button style={{boxShadow:"inset 6px 6px 6px rgba(255, 255, 255, 0.2)"}} disabled={loading} onClick={handleSave} className={`${loading ? "bg-gray-100  flex items-center justify-center" : "bg-gradient-to-br from-customGreen to-customBlue"} text-white  w-max mx-auto py-3 px-20   mt-20 mb-10  rounded-full text-base font-medium shadow-md   shadow-customGreen `} >
           {
             loading ?
               <img src={spinner} alt="Loading" width={28} height={28} className="animate-spin " /> : "Submit"
